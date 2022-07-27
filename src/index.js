0bf9cb8d445303f4934b3192b61347e9bc875e90
andre-sqr <andrebsqr@gmail.com>
2022-07-27 00:22:15 -0300

    fixed: nome do import alterado e retirado função connect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { router } from "./router/router.js";
-import connect from "./db/connect.js";
+import db from "./db/connect.js";
+
 const app = express()
 
-connect()
 app.use(express.json())
 app.use(router)
 app.listen(3000, ()=> {
