c32fce35b486109038a954a3cd9d00f3eda3d727
andre-sqr <andrebsqr@gmail.com>
2022-07-27 11:58:54 -0300

    model cliente iniciada com métodos vazios

---
+++ src/model/clienteModel.js
@@ -0,0 +1,37 @@
+class Cliente {
+    constructor(id, nome, email, cpf, telefone, cep, senha) {
+        this.id = idObj 
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.telefone = telefone
+        this.cep = cep
+        this.senha = senha
+    }
+
+    inserirCliente = () => {
+
+    }
+
+    removerCliente = () => {
+
+    }
+
+    buscarClientesTodos = () => {
+        
+    }
+
+    buscarClienteNome = (nome) => {
+
+}
+
+    buscarClienteCPF = (CPF) => {
+
+    }
+
+    atualizarCliente = () => {
+
+    }
+}
+
+export default Cliente
