4dbd677ebc33c6addb03e56aed409fa26a17cbd0
Wellison Sales <wellisonsalesbr@gmail.com>
2022-07-27 18:40:33 -0300

    populando entidade estoque

---
+++ src/Controller/EstoqueController.js
@@ -0,0 +1,2 @@
+import { Database } from "sqlite3";
+
--- src/database/criar-e-popular.js
+++ src/database/criar-e-popular.js
@@ -2,118 +2,36 @@ import sqlite3 from 'sqlite3'
 sqlite3.verbose()
 const db = new sqlite3.Database('olimpia.db');
 
-const LIVROS_SCHEMA = `
-CREATE TABLE IF NOT EXISTS "LIVROS" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
-    "titulo" text,
-    "autor" text,
-    "genero" text,
-    "formato" text,
-    "valor" real,
-    "idioma" text,
-    "numeroPaginas" int
-  );
-`
-
-function criaTabelaLivros() {
-    db.run(LIVROS_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de livros");
-    });
-}
-
-const CLIENTES_SCHEMA = `
-CREATE TABLE IF NOT EXISTS "CLIENTES" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
-    "nome" text,
-    "email" text,
-    "cpf" text,
-    "telefone" text,
-    "senha" text,
-    "cep" text
-  );
-`
-
-function criaTabelaClientes() {
-    db.run(CLIENTES_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de clientes");
-    });
-}
-
-const FUNCIONARIOS_SCHEMA = `
-CREATE TABLE IF NOT EXISTS "FUNCIONARIOS" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
-    "nome" text,
-    "email" text,
-    "cpf" text,
-    "telefone" text,
-    "senha" text,
-    "cargo" text
-  );
-`
-
-function criaTabelaFuncionarios() {
-    db.run(FUNCIONARIOS_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de funcionários");
-    });
-}
-
-
-const FORNECEDORES_SCHEMA = `
-CREATE TABLE IF NOT EXISTS "FORNECEDORES" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
-    "nome" text,
-    "email" text,
-    "cnpj" text,
-    "telefone" text
-  );
-`
-function criaTabelaFornecedores() {
-    db.run(FORNECEDORES_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de fornecedores");
-    });
-}
-
-
-const PAGAMENTOS_SCHEMA = `
-CREATE TABLE IF NOT EXISTS "PAGAMENTOS" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
-    "cliente" int,
-    "forma_pagamento" text,
-    "valor" real,
-    "parcelamento" text,
-    "status" text,
-    "data" text
-  );
-  
-`
-
-function criaTabelaPagamentos() {
-    db.run(PAGAMENTOS_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de pagamentos");
-    });
-}
-
-
 const ESTOQUE_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "ESTOQUE" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
-    "produto" int,
-    "quantidade" int,
-    "fornecedor" int
+    "id_estoque" int PRIMARY KEY AUTO_INCREMENT,
+    "livro" varchar (64)
+    "quantidade" varchar (64)
+    "entrada" varchar (64)
+    "cod_barras" varchar (64)
+    "status" varchar (64)
   );
+`
+
+const popularEstoque = `
+INSERT INTO ESTOQUE (id_estoque, livro, quantidade, entrada, cod_barras, status)
+VALUES 
+    (101, 'Atirei o pau no gato', '54', '23/05/2002', '03242789734', 'ativo'),
 `
 
 function criaTabelaEstoque() {
     db.run(ESTOQUE_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de estoque");
+        if (error) console.log("Erro ao criar tabela de livros");
     });
 }
 
+function popularTabelaEstoque() {
+    db.run(popularEstoque, (error)=> {
+       if (error) console.log("Erro ao popular tabela de usuários");
+    })
+}
+
 db.serialize( ()=> {
-    criaTabelaLivros()
-    criaTabelaClientes()
-    criaTabelaFuncionarios()
-    criaTabelaFornecedores()
-    criaTabelaPagamentos()
     criaTabelaEstoque()
+    popularTabelaEstoque()
 })
