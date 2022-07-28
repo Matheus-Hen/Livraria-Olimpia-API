ae0defb20c8d2a7617676e2fb7ae9266626ed7a9
est8r <estermessias6@gmail.com>
2022-07-28 18:36:04 -0300

    inicializada a criação da classe sem métodos

--- src/model/fornecedoresModel.js
+++ src/model/fornecedoresModel.js
@@ -1,1 +1,13 @@
+class Fornecedores {
+  constructor(id, nome, cnpj, produto, email, telefone, endereco) {
+    this.id = id;
+    this.nome = nome;
+    this.email = email;
+    this.cnpj = cnpj;
+    this.produto = produto;
+    this.telefone = telefone;
+    this.endereco = endereco;
+  }
+}
 
+export default Fornecedores;
