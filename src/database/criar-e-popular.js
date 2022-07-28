e940614be59a4ab3706c2c8e8abd8699588ab367
lisandradecassia <lisandradecassia@gmail.com>
2022-07-28 16:43:05 -0300

    FIX: Corrigi sintaxe da criação e população de tabelas.

--- src/database/criar-e-popular.js
+++ src/database/criar-e-popular.js
@@ -2,9 +2,10 @@ import sqlite3 from 'sqlite3'
 sqlite3.verbose()
 const db = new sqlite3.Database('olimpia.db');
 
+// ******************LIVROS*************************
 const LIVROS_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "LIVROS" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "titulo" text,
     "autor" text,
     "genero" text,
@@ -17,13 +18,14 @@ CREATE TABLE IF NOT EXISTS "LIVROS" (
 
 function criaTabelaLivros() {
     db.run(LIVROS_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de livros");
+        if (error) console.log(`Erro na criação da tabela livros: ${error.message}`);
     });
 }
 
+// ******************CLIENTES*************************
 const CLIENTES_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "CLIENTES" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "nome" text,
     "email" text,
     "cpf" text,
@@ -35,13 +37,14 @@ CREATE TABLE IF NOT EXISTS "CLIENTES" (
 
 function criaTabelaClientes() {
     db.run(CLIENTES_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de clientes");
+        if (error) console.log(`Erro na criação da tabela clientes: ${error.message}`);
     });
 }
 
+// ******************FUNCIONÁRIOS*************************
 const FUNCIONARIOS_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "FUNCIONARIOS" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "nome" text,
     "email" text,
     "cpf" text,
@@ -53,14 +56,14 @@ CREATE TABLE IF NOT EXISTS "FUNCIONARIOS" (
 
 function criaTabelaFuncionarios() {
     db.run(FUNCIONARIOS_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de funcionários");
+        if (error) console.log(`Erro na criação da tabela funcionarios: ${error.message}`);
     });
 }
 
-
+// ******************FORNECEDORES*************************
 const FORNECEDORES_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "FORNECEDORES" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "nome" text,
     "email" text,
     "cnpj" text,
@@ -69,15 +72,15 @@ CREATE TABLE IF NOT EXISTS "FORNECEDORES" (
 `
 function criaTabelaFornecedores() {
     db.run(FORNECEDORES_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de fornecedores");
+        if (error) console.log("`Erro na criação da tabela fornecedores: ${error.message}`");
     });
 }
 
-
+// ******************PAGAMENTOS*************************
 const PAGAMENTOS_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "PAGAMENTOS" (
-    "id_pagamentos" int PRIMARY KEY AUTOINCREMENT,
-    "cliente" text,
+    "id_pagamentos" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "cliente" integer,
     "forma_pagamento" text,
     "valor" real,
     "parcelamento" text,
@@ -89,37 +92,38 @@ CREATE TABLE IF NOT EXISTS "PAGAMENTOS" (
 const popularPagamentos = `
 INSERT INTO PAGAMENTOS (id_pagamentos, cliente, forma_pagamento, valor, parcelamento, status, data)
 VALUES 
-    (002022, 'luana silva de alencar', 'pix', '120', '0', 'pago', '20/08/2022'),
-    (002023, 'pedro josé Barros', 'cartao', '89', '2', 'pago', '15/08/2022'),
-    (002024, 'marcos santos', 'boleto', '180', '1', 'pago', '02/08/2022'),
-    (002025, 'amanda tainá Rosa', 'pix', '59', '0', 'pago', '17/06/2022'),
-    (002026, 'ana Julia maria', 'cartao', '59', '0', 'pago', '17/06/2022')
+    (002022, 'luana silva de alencar', 'pix', 120, 0, 'pago', '20/08/2022'),
+    (002023, 'pedro josé Barros', 'cartao', 89, 2, 'pago', '15/08/2022'),
+    (002024, 'marcos santos', 'boleto', 180, 1, 'pago', '02/08/2022'),
+    (002025, 'amanda tainá Rosa', 'pix', 59, 0, 'pago', '17/06/2022'),
+    (002026, 'ana Julia maria', 'cartao', 59, 0, 'pago', '17/06/2022')
 `
 
 function criaTabelaPagamentos() {
     db.run(PAGAMENTOS_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de pagamentos!");
+        if (error) console.log(`Erro na criação da tabela pagamentos: ${error.message}`);
     });
 }
 
 function popularTabelaPagamentos() {
     db.run(popularPagamentos, (error)=> {
-       if (error) console.log("Erro ao popular tabela de pagamentos!");
+        if (error) console.log(`Erro ao popular tabela pagamentos: ${error.message}`);
     })
 }
 
+// ******************ESTOQUE*************************
 const ESTOQUE_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "ESTOQUE" (
-    "id" int PRIMARY KEY AUTO_INCREMENT,
-    "produto" int,
-    "quantidade" int,
-    "fornecedor" int
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "produto" integer,
+    "quantidade" integer,
+    "fornecedor" integer
   );
 `
 
 function criaTabelaEstoque() {
     db.run(ESTOQUE_SCHEMA, (error)=> {
-        if (error) console.log("Erro ao criar tabela de estoque");
+        if (error) console.log(`Erro na criação da tabela estoque: ${error.message}`);
     });
 }
 
