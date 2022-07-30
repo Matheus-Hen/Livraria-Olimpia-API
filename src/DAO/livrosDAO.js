35eb7acdf606db9ab3942d9d80da23e2ad6772eb
nathuchoa <nathaliauchoa@hotmail.com>
2022-07-30 23:27:22 +0100

    Iniciando a livrosDAO.

---
+++ src/DAO/livrosDAO.js
@@ -0,0 +1,40 @@
+import db from '../database/connect.js'
+
+const daoLivros = {
+
+    listarLivros : ()=>{
+        const LISTAR_LIVROS = `
+        SELECT * FROM LIVROS
+        `
+        return new Promise((resolve, reject)=>{
+            db.all(LISTAR_LIVROS, (error,row)=>{
+                if(error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
+
+    cadastroLivro : (livro)=>{
+        const ADD_LIVRO = `
+        INSERT INTO LIVROS (id_livro, titulo, autor, genero, formato, valor, idioma, numeroPagina)
+        VALUES (?,?,?)
+        `
+        return new Promise((resolve, reject)=>{
+            db.run(ADD_LIVRO,
+                livro.idLivro, livro.titulo, livro.autor, livro.genero, livro.formato, livro.valor, livro.idioma, livro.numeroPaginas,
+                (error)=>{
+                    if(error)
+                        reject(error)
+                    else
+                        resolve(livro)
+                }
+            )
+        })
+    },
+
+    
+}
+
+export default daoLivros
