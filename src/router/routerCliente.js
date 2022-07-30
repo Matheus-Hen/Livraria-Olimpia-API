074aa5e06e6e86838221d421cf4e4a5da9659a69
andre-sqr <andrebsqr@gmail.com>
2022-07-29 21:03:50 -0300

    router iniciada com métodos de busca

---
+++ src/router/routerCliente.js
@@ -0,0 +1,11 @@
+import clienteController from "../controller/clienteController.js";
+
+const routerCliente = (app)=> {
+
+    app.get('/clientes', clienteController.buscarClientes)
+    app.get('/clientes/nome/:nome', clienteController.buscarClientePeloNome)
+    app.get('/clientes/cpf/:cpf', clienteController.buscarClientePeloCPF)
+    app.get('/clientes/email/:email', clienteController.buscarClientePeloEmail)
+}
+
+export default routerCliente
