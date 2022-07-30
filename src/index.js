4ee820e234209f6c47a65a810183dd386d4e7792
est8r <estermessias6@gmail.com>
2022-07-29 22:17:34 -0300

    inserindo dados atraves do método run

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import { router } from "./router/router.js";
 import db from "./database/connect.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(router)
-app.listen(3000, ()=> {
-    console.log('Servidor inicializado')
-})
+app.use(express.json());
+app.use(router);
+app.listen(3000, () => {
+  console.log("Servidor inicializado");
+});
