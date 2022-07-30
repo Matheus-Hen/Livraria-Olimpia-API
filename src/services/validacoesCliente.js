e345ba3d65c6b5081262b6c4f72d4cd7a0f7300f
andre-sqr <andrebsqr@gmail.com>
2022-07-30 14:57:10 -0300

    adicionado função de criar clientes no services

---
+++ src/services/validacoesCliente.js
@@ -0,0 +1,12 @@
+export const criaCliente = (nome, email, cpf, telefone, cep, senha)=> {
+// TODO: Funções de validação
+
+return {
+    "nome": nome,
+    "email": email,
+    "cpf": cpf,
+    "telefone": telefone,
+    "cep": cep,
+    "senha": senha
+}
+}
