2cbe8f68c7c71b7f1ccf54de8eda87bb71d220cc
est8r <estermessias6@gmail.com>
2022-07-31 03:24:10 -0300

    adicionado o método de criação de fornecedores

---
+++ src/Controller/fornecedoresController.js
@@ -0,0 +1,27 @@
+import Fornecedores from '../model/fornecedoresModel.js'
+import { criaFornecedor } from '../services/validacoesFornecedores.js'
+
+const fornecedoresController = {
+
+    //CRIA / INSERE
+    criarNewFornecedor: async (req, res)=> {
+      const body = req.body
+      const modelFornecedor = new Fornecedores()
+      try {
+        const novoFornecedor = criaFornecedor(body.nome, body.cnpj, body.produto,
+        body.email, body.telefone, body.endereco, body.cep)
+        await modelFornecedor.insereFornecedores(novoFornecedor)
+          res.json(
+            {"msg": "Fornecedor inserido",
+              "cliente": novoFornecedor,
+              "erro": false}
+          )   
+            } catch (error) {
+              res.json(
+               {"msg": error.message,
+                "erro": true}
+               )
+            }
+    },
+    
+}
