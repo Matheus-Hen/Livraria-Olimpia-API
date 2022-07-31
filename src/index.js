1fa900e75adb9dd71562a821e69e2b06dd11dae7
est8r <estermessias6@gmail.com>
2022-07-31 03:57:07 -0300

    importando router na index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { router } from "./router/router.js";
+import fornecedores from "./router/fornecedoresRouter.js"
 import db from "./database/connect.js";
 
 const app = express();
 
 app.use(express.json());
+fornecedores(app)
 app.use(router);
 app.listen(3000, () => {
   console.log("Servidor inicializado");
