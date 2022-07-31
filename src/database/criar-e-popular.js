3a7fd21dcc8534b586e0ba9b4d3cb4da56f96a00
est8r <estermessias6@gmail.com>
2022-07-31 04:32:55 -0300

    adicionando mais dados ao banco de dados

--- src/database/criar-e-popular.js
+++ src/database/criar-e-popular.js
@@ -76,11 +76,15 @@ CREATE TABLE IF NOT EXISTS "FORNECEDORES" (
 const populando_fornecedores = `
     INSERT INTO FORNECEDORES (id, nome, cnpj, produto, email, telefone, endereco, cep)
 VALUES 
-    (001, 'PAPERSHIFT', '03546726000111', 'Papel', 'compras@papershift.com.br', '55 19 23768254', 'Avenida Barcelona, 1405 - Agua Branca, Campinas - SP', '03945080'),
+    (001, 'PAPERSHIFT', '03546726000111', 'Artigos de Papelaria', 'compras@papershift.com.br', '55 19 23768254', 'Avenida Barcelona, 1405 - Agua Branca, Campinas - SP', '03945080'),
 
-    (002, 'A Página', '01795809000110', 'Livros', 'compras@apagina.com.br', '55 41 32135643', 'Rua Major Fabriciano do Rego Barros, 1050 - Hauer, Curitiba - PR', '81630260')
+    (002, 'A Página', '01795809000110', 'Livros', 'compras@apagina.com.br', '55 41 32135643', 'Rua Major Fabriciano do Rego Barros, 1050 - Hauer, Curitiba - PR', '01830260'),
 
+    (003, 'Trucks LTDA', '05938204710110', 'Chaveiros', 'compras@trucks.com.br', '55 32 77638271', 'Rua Jão Pires de Lima, 405 - Santos, Belo Horizonte - MG', '01632360'),
 
+    (004, 'Safe&Sound', '082718275600110', 'Artigos Musicais', 'compras@safeandsound.com.br', '55 65 55789452', 'Rua Vicente Martins, 200 - Pereira, Cuiabá - MT', '08354780'),
+
+    (005, 'Bookstan', '01746378240110', 'Livros', 'compras@bookstan.com.br', '55 98 88748278', 'Rua Graciliano Ramos, 400 - Assis, São Luís - MA', '04985570')
     `;
 
 function criaTabelaFornecedores() {
