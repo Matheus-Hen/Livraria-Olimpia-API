68948fe7680c61dcb7fcec3fd44fb157b2cff42f
nathuchoa <nathaliauchoa@hotmail.com>
2022-07-31 23:50:16 +0100

    Atualização das routers

--- src/router/routerLivros.js
+++ src/router/routerLivros.js
@@ -3,14 +3,13 @@ import livroController from "../Controller/livrosController.js";
 const routerLivros = (app)=> {
 
     app.get('/livros', livroController.listarLivros)
-    app.get('/livro/titulo/:titulo', livroController.buscarTituloLivro)
-    app.get('/livro/genero/:genero', livroController.buscarGeneroLivro)
-    app.get('/livro/autor/:autor', livroController.buscarAutorLivro)
-    app.get('/livro/idioma/:idioma', livroController.buscarIdiomaLivro)
-    app.post('/livro/cadastroLivro/:cadastroLivro', livroController.cadastrarLivro)
-    app.delete('/livro/removerLivro/:removerLivro', livroController.removerLivro)
-    //app.put - atualizar valor (?)
-    //app.patch - verificar no estoque (?)
+    app.get('/livro/titulo/:titulo', livroController.buscaLivro)
+    app.get('/livro/genero/:genero', livroController.buscaGenero)
+    app.get('/livro/autor/:autor', livroController.buscaAutor)
+    app.get('/livro/idioma/:idioma', livroController.buscaIdioma)
+    app.post('/livro/cadastroLivro/:cadastroLivro', livroController.cadastroNovoLivro)
+    app.delete('/livro/removerLivro/:removerLivro', livroController.deletaLivro)
+    app.put('/livro/atualizarLivro/:atualizarLivro', livroController.atualizarValorLivro)
 }
 
-export default routerLivros;
+export default routerLivros
