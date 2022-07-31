6b31f11e90a138264806f777a39933c3a817fb3e
lisandradecassia <lisandradecassia@gmail.com>
2022-07-30 22:40:53 -0300

    ADD: rota de busca de pagamentos.

---
+++ src/router/routerPagamentos.js
@@ -0,0 +1,15 @@
+import pagamentosController from "../controller/pagamentosController.js";
+
+const routerPagamentos = (app) => {
+
+    app.get('/pagamentos', pagamentosController.pegaPagamentosTodos)
+    app.get('/pagamentos/idPagamentos/:idPagamentos', pagamentosController.pegaPagamentosId)
+    app.get('/pagamentos/status/:status', pagamentosController.pegaPagamentosStatus)
+    app.get('/pagamentos/formaDePagamento/:formaDePagamento', pagamentosController.pegaPagamentosForma)
+    app.get('/pagamentos/data/:data', pagamentosController.pegaPagamentosData)
+    app.get('/pagamentos/valor/:valor', pagamentosController.pegaPagamentosValor)
+    app.get('/pagamentos/parcelamento/:parcelamento', pagamentosController.pegaPagamentosParcelamento)
+    app.get('/pagamentos/cliente/:cliente', pagamentosController.pegaPagamentosCliente)
+}
+
+export default routerPagamentos
