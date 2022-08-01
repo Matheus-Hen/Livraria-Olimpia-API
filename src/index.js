3aa948245d8ea2f924d6a7cb09a620b69041232f
nathuchoa <nathaliauchoa@hotmail.com>
2022-08-01 01:23:03 +0100

    Adicionando routerLivros na index.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { router } from "./router/router.js";
-import db from "./database/connect.js";
+import  routerLivros   from "./router/routerLivros.js";
 
 const app = express()
 
 app.use(express.json())
+
+routerLivros(app)
+
 app.use(router)
 app.listen(3000, ()=> {
     console.log('Servidor inicializado')
