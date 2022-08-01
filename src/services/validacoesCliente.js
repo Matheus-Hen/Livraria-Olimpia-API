cf250331a1ae9868110095c9701a986bccb2f9ae
andre-sqr <andrebsqr@gmail.com>
2022-08-01 00:36:16 -0300

    feat: validações de email, senha e cpf do cliente

--- src/services/validacoesCliente.js
+++ src/services/validacoesCliente.js
@@ -1,28 +1,38 @@
-import seteDigitos from "./filtroDeValidacao"
-import verificaArroba from "./filtroDeValidacao"
-import validaCPF from "./filtroDeValidacao"
+import { seteDigitos, verificaArroba, validaCPF } from "./filtroValidacao.js"
 
-//valida senha
-const seteDigitos = (senha)=>{
+const validarSenha = (senha)=>{
     if(!seteDigitos(senha)){
         throw new Error("A senha precisa ter 7 ou mais caracteres")
     }
 }
 
-//valida email 
-const verificaArroba = (email)=>{
-    if(!verificaArroba(email)){
+const validarEmail = (email)=>{
+    if(verificaArroba(email)){
         return email
     }else{
         throw new Error("Algo está faltando em seu email, digite corretamente.")
     }
 }
 
-//valida cpf
-const validaCPF = (cpf) => {
-    if(!validaCPF, (cpf)){
+const validarCPF = (cpf) => {
+    if(validaCPF (cpf)){
         return cpf
     }else{
         throw new Error("Verifique o campo do cpf")
     }
 }
+
+export const criaCliente = (nome, email, cpf, telefone, senha, cep)=> {
+    validarEmail(email)
+    validarSenha(senha)
+    validarCPF(cpf)
+
+    return {
+        "nome": nome,
+        "email": email,
+        "cpf": cpf,
+        "telefone": telefone,
+        "senha": senha,
+        "cep": cep
+    }
+    } 
