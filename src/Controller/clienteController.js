8df6f349696a412bf242790402372282481fc3ce
andre-sqr <andrebsqr@gmail.com>
2022-08-02 18:21:27 -0300

    feat: tratamento de erros na controller

--- src/Controller/clienteController.js
+++ src/Controller/clienteController.js
@@ -1,66 +1,100 @@
 import Cliente from "../model/clienteModel.js";
 import { criaCliente } from "../services/validacoesCliente.js";
 
+const modelCliente = new Cliente()
 
 const clienteController = {
     
     buscarClientes : async (req, res)=> {
-        const modelCliente = new Cliente()
-        const todosClientes = await modelCliente.buscarClientesTodos()
-        
-        res.json({
-            "clientes": todosClientes,
-            "erro": false
-        })
+        try {
+            const todosClientes = await modelCliente.buscarClientesTodos()
+            
+            res.json({
+                "clientes": todosClientes,
+                "erro": false
+            })
+        } catch (error) {
+            res.json(
+                {"msg": error.message,
+                "erro": true}
+                )
+        }
     },
     
     buscarClientePeloNome: async (req, res)=> {
         const nome = req.params.nome
-        const modelCliente = new Cliente()
-        const resultadoBusca = await modelCliente.buscarClienteNome(nome)
-        
-        res.json({
-            "cliente": resultadoBusca,
-            "erro": false
-        })
+        try { 
+            const resultadoBusca = await modelCliente.buscarClienteNome(nome)
+            
+            res.json({
+                "cliente": resultadoBusca,
+                "erro": false
+            })
+        } catch (error) {
+            res.json(
+                {"msg": error.message,
+                "erro": true}
+                )
+        }
     },
     
     buscarClientePeloCPF: async (req, res)=> {
         const cpf = req.params.cpf
-        const modelCliente = new Cliente()
-        const resultadoBusca = await modelCliente.buscarClienteCPF(cpf) 
-        
-        res.json({
-            "cliente": resultadoBusca,
-            "erro": false
-        })
+
+        try {
+            const resultadoBusca = await modelCliente.buscarClienteCPF(cpf) 
+            
+            res.json({
+                "cliente": resultadoBusca,
+                "erro": false
+            })
+        } catch (error) {
+            res.json(
+                {"msg": error.message,
+                "erro": true}
+                )
+        }
     },
     
     buscarClientePeloEmail: async (req, res)=> {
         const email = req.params.email
-        const modelCliente = new Cliente()
-        const resultadoBusca = await modelCliente.buscarClienteEmail(email)
-        
-        res.json({
-            "cliente": resultadoBusca,
-            "erro": false
-        })
+
+        try {
+            const resultadoBusca = await modelCliente.buscarClienteEmail(email)
+            
+            res.json({
+                "cliente": resultadoBusca,
+                "erro": false
+            })
+        } catch (error) {
+            res.json(
+                {"msg": error.message,
+                "erro": true}
+                )
+        }
     },
     
     buscarClientePeloID: async (req, res)=> {
         const id = req.params.id
-        const modelCliente = new Cliente()
-        const resultadoBusca = await modelCliente.buscarClienteId(id)
-        
-        res.json({
-            "cliente": resultadoBusca,
-            "erro": false
-        })
+
+        try {
+            const resultadoBusca = await modelCliente.buscarClienteId(id)
+            
+            res.json({
+                "cliente": resultadoBusca,
+                "erro": false
+            })
+        } catch (error) {
+            res.json(
+                {"msg": error.message,
+                "erro": true}
+                )
+        }
     },
     
     criarNovoCliente: async (req, res)=> {
         const body = req.body
-        const modelCliente = new Cliente()
+
         try {
             const novoCliente = criaCliente(body.nome, body.email, body.cpf, 
                 body.telefone, body.cep, body.senha)
@@ -80,45 +114,45 @@ const clienteController = {
                     }
                 },
                 
-                deletaCliente: async (req, res)=> {
-                    const modelCliente = new Cliente()
-                    const id = req.params.id
-                    try { 
-                        await modelCliente.removerCliente(id)
+    deletaCliente: async (req, res)=> {
+        const id = req.params.id
+
+        try { 
+            await modelCliente.removerCliente(id)
                         
-                        res.json(
-                            {"msg": "Cliente deletado com sucesso",
-                            "erro": false}
-                            )
-                        } catch (error) {
-                            res.json(
-                                { "msg": error.message,
-                                "erro": true}
-                                )
-                            }
-                        },
+            res.json(
+                {"msg": "Cliente deletado com sucesso",
+                 "erro": false}
+                )
+            } catch (error) {
+                res.json(
+                    { "msg": error.message,
+                    "erro": true}
+                     )
+                }
+            },
                         
-                        atualizaCliente: async (req, res)=> {
-                            const modelCliente = new Cliente()
-                            const id = req.params.id
-                            const body = req.body
-                            try { 
-                                const clienteAtualizado = criaCliente(body.nome, body.email, body.cpf,
-                                    body.telefone, body.senha, body.cep)
-                                    await modelCliente.atualizarCliente(id, clienteAtualizado)
-                                    res.json(
-                                        {"msg": "Cliente atualizado com sucesso",
-                                        "cliente": clienteAtualizado,
-                                        "erro": false}
-                                        )
-                                    } catch (error) {
-                                        res.json(
-                                            {"msg": error.message,
-                                            "erro": true}
-                                            )
-                                        }
-                                    }
-                                }
+    atualizaCliente: async (req, res)=> {
+        const id = req.params.id
+        const body = req.body
+        
+        try { 
+            const clienteAtualizado = criaCliente(body.nome, body.email, body.cpf,
+            body.telefone, body.senha, body.cep)
+            await modelCliente.atualizarCliente(id, clienteAtualizado)
+            res.json(
+                {"msg": "Cliente atualizado com sucesso",
+                 "cliente": clienteAtualizado,
+                  "erro": false}
+                )
+            } catch (error) {
+                res.json(
+                     {"msg": error.message,
+                      "erro": true}
+                    )
+                }
+            }
+        }
                                 
                                 
                                 export default clienteController
