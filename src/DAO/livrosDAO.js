035bdeab09110cfdf523e31c15cfb8097c535a77
nathuchoa <nathaliauchoa@hotmail.com>
2022-08-02 19:52:43 +0100

    Adicionando métodos na DAO -get, post, delete - ok

--- src/DAO/livrosDAO.js
+++ src/DAO/livrosDAO.js
@@ -2,28 +2,13 @@ import db from '../database/connect.js'
 
 const daoLivros = {
 
-    listarLivros : ()=>{
-        const LISTAR_LIVROS = `
-        SELECT * FROM LIVROS
-        `
-        return new Promise((resolve, reject)=>{
-            db.all(LISTAR_LIVROS, (error,row)=>{
-                if(error)
-                    reject(error)
-                else
-                    resolve(row)
-            })
-        })
-    },
-
     cadastrarLivro : (livro)=>{
         const ADD_LIVRO = `
-        INSERT INTO LIVROS (idLivro, titulo, autor, genero, formato, valor, idioma, numeroPaginas)
-        VALUES (?,?,?,?,?,?,?,?)
+        INSERT INTO LIVROS (titulo, autor, genero, formato, valor, idioma, numeroPaginas)
+        VALUES (?,?,?,?,?,?,?)
         `
         return new Promise((resolve, reject)=>{
-            db.run(ADD_LIVRO,
-                livro.idLivro, 
+            db.run(ADD_LIVRO, 
                 livro.titulo, 
                 livro.autor, 
                 livro.genero, 
@@ -40,6 +25,87 @@ const daoLivros = {
             )
         })
     },
+
+    listarLivros : ()=>{
+        const LISTAR_LIVROS = `
+        SELECT * FROM LIVROS
+        `
+        return new Promise((resolve, reject)=>{
+            db.all(LISTAR_LIVROS, (error,row)=>{
+                if(error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
+
+    buscaLivro : (titulo)=>{
+        const BUSCA_LIVRO = `
+        SELECT * FROM LIVROS 
+        WHERE titulo = ?
+        `
+        return new Promise((resolve, reject)=>{
+            db.get(BUSCA_LIVRO, titulo, (error, row)=>{
+                if(error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
+
+    buscaLivroId : (idLivro)=>{
+        const BUSCA_LIVRO = `
+        SELECT * FROM LIVROS 
+        WHERE idLivro = ?
+        `
+        return new Promise((resolve, reject)=>{
+            db.get(BUSCA_LIVRO, idLivro, (error, row)=>{
+                if(error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
+
+    removeLivro : (idLivro) => {
+        const DELETA_LIVRO = `
+        DELETE FROM LIVROS 
+        WHERE idLivro = ?
+        `
+
+        return new Promise ((resolve, reject) => {
+            db.run(DELETA_LIVRO, idLivro, (error, row)=> {
+                if (error)
+                    reject (error)
+                else
+                    resolve (row)
+            })
+        })
+
+    },
+
+    atualizaValorLivro : (idLivro, novoValor)=> {
+        const ATUALIZA_VALOR = `
+        UPDATE LIVROS
+        SET valor = ?
+        WHERE idLivro = ?
+        `
+        return new Promise((resolve, reject)=> {
+            db.run(ATUALIZA_VALOR, 
+                novoValor.valor, idLivro,
+                (error)=>{
+                    if (error)
+                        reject(error)
+                    else
+                        resolve(novoValor)
+                })
+        })
+    }
+
+    
 
     
 }
