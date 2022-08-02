a8f8e0b3121a03325030d36cd77a64f7db98b62d
nathuchoa <nathaliauchoa@hotmail.com>
2022-08-02 21:21:58 +0100

    Rotas atualizadas (post, delele, put)

--- src/router/routerLivros.js
+++ src/router/routerLivros.js
@@ -4,12 +4,16 @@ const routerLivros = (app)=> {
 
     app.get('/livros', livroController.listarLivros)
     app.get('/livro/titulo/:titulo', livroController.buscaLivro)
+    app.get('/livro/idLivro/:idLivro', livroController.buscaLivroId)
     app.get('/livro/genero/:genero', livroController.buscaGenero)
     app.get('/livro/autor/:autor', livroController.buscaAutor)
     app.get('/livro/idioma/:idioma', livroController.buscaIdioma)
-    app.post('/livro/cadastroLivro/:cadastroLivro', livroController.cadastroNovoLivro)
-    app.delete('/livro/removerLivro/:removerLivro', livroController.deletaLivro)
-    app.put('/livro/atualizarLivro/:atualizarLivro', livroController.atualizarValorLivro)
+
+    app.post('/livro/cadastroLivro', livroController.cadastroNovoLivro)
+
+    app.delete('/livro/idLivro/:idLivro', livroController.removeLivro)
+
+    app.put('/livro/livro/:idLivro', livroController.atualizaLivro)
 }
 
-export default routerLivros
+export default routerLivros 
