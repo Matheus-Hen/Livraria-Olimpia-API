67e82f964ce35107f3c9392faa7cb06a778ffe78
est8r <estermessias6@gmail.com>
2022-08-12 03:08:05 -0300

    iniciada refatoração das validações de dados

--- src/services/filtroValidacao.js
+++ src/services/filtroValidacao.js
@@ -1,72 +1,14 @@
-// filtra a senha que deve possuir 7 caracteres
-const seteDigitos = (senha) => {
-  return senha?.length >= 7
-}
-//filtro se possui o @ no email
-const verificaArroba = (email) => {
-  return email?.includes("@")
-}
+const Validacoes = {
+  _validaCNPJ: (cnpj) => {
+    if (cnpj) {
+      if (cnpj.length < 14) {
+        throw new Error('O CNPJ precisa ter 14 caracteres');
+      }
+    } else {
+      throw new Error('Insira um CNPJ com 14 caracteres');
+    }
+    return cnpj;
+  },
+};
 
-//filtro de cpf
-const validaCPF = (cpf) => {
-  var Soma = 0
-  var Resto
-
-  var strCPF = String(cpf).replace(/[^\d]/g, '')
-
-  // limpando o parâmetro cpf removendo caracteres
-  console.log(strCPF, strCPF.length);
-  if (strCPF.length !== 11) return false;
-
-  // verificando se o parâmetro srtCPF não repete os números
-
-  if ([
-    '00000000000',
-    '11111111111',
-    '22222222222',
-    '33333333333',
-    '44444444444',
-    '55555555555',
-    '66666666666',
-    '77777777777',
-    '88888888888',
-    '99999999999',
-  ].indexOf(strCPF) !== -1)
-    return false
-
-  return true
-}
-
-
-export const validarSenha = (senha)=>{
-  if(!seteDigitos(senha)){
-      throw new Error("A senha precisa ter 7 ou mais caracteres")
-  }
-}
-
-export const validarEmail = (email)=>{
-  if(verificaArroba(email)){
-      return email
-  }else{
-      throw new Error("Algo está faltando em seu email, digite corretamente.")
-  }
-}
-
-export const validarCPF = (cpf) => {
-  if(validaCPF (cpf)){
-      return cpf
-  }else{
-      throw new Error("Verifique o campo do cpf")
-  }
-}
-
-export const validaCNPJ = (cnpj)=>{
-  if(cnpj){
-      if(cnpj.length < 14){
-          throw new Error("O CNPJ precisa ter 14 caracteres")
-      }          
-  }else{
-      throw new Error("Insira um CNPJ com 14 caracteres")
-  }
-  return cnpj
-}
+export default Validacoes;
